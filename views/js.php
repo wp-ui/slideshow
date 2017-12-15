12a3fee293553209fa28aadc0a5b8987bc9f6598
Makis Tracend <makis.tracend@gmail.com>
2017-12-16 01:51:31 +0200

    non-conflicting instances in global namespace

--- views/js.php
+++ views/js.php
@@ -1,8 +1,13 @@
 /* dynamically generated stylesheet */
 <?php
 // variables
+$key = str_replace("#ui-", "", $options['el']);
 ?>
+// global namespace
+if(typeof ui != "object") ui = {};
 jQuery(document).ready(function(){
 	var view = new Backbone.UI.Slideshow(<?php echo json_encode($options, JSON_NUMERIC_CHECK) ?>);
 	view.render();
+	// save in the global namespace
+	ui['<?=$key?>'] = view;
 });
